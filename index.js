10ed1bd6088f6bc349b0e713d3e1c2fc02a4a1db
Tyler Brauner <tylerbrauner@gmail.com>
2023-11-03 17:35:30 -0500

    Expense Tracker is now fully functional

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ function getData() {
     let name = document.getElementById('name_textbox').value;
     let date = document.getElementById('date_box').value;
     let amount = document.getElementById('amount_box').value;
+    if(name == '') {
+        return window.alert("Please Fill Out All Fields");
+    } else if(date == '') {
+        return window.alert("Please Fill Out All Fields");
+    } else if(amount == '') {
+        return window.alert("Please Fill Out All Fields");
+    }
     let tableSection = document.getElementById('table');
     expense++;
     let newName = document.createElement('div');
@@ -11,6 +18,7 @@ function getData() {
     let newButton = document.createElement('input');
     newButton.value = 'X';
     newButton.type = 'button';
+    newButton.addEventListener('click', removeData);
     newName.classList.add('table_input');
     newDate.classList.add('table_input');
     newAmount.classList.add('table_input');
@@ -25,4 +33,23 @@ function getData() {
     tableSection.appendChild(newDate);
     tableSection.appendChild(newAmount);
     tableSection.appendChild(newButton);
+    return;
+}
+
+function removeData(test) {
+    expense--;
+    const value = test.target.id;
+    const numRegex = /\d+/;
+    const num = value.match(numRegex);
+    const number = num[0];
+    const startTable = document.getElementById('table');
+    const childName = document.getElementById(`name${number}`);
+    const childDate = document.getElementById(`date${number}`);
+    const childAmount = document.getElementById(`amount${number}`);
+    const childButton = document.getElementById(`button${number}`);
+    startTable.removeChild(childButton);
+    startTable.removeChild(childName);
+    startTable.removeChild(childDate);
+    startTable.removeChild(childAmount);
+    return;
 }
